d21ba252f48a749f0dd00ea1ddd9d2dcec09cf18
Your Name <you@example.com>
2020-08-05 20:40:12 +0100

    updated font and opacity

--- config.def.h
+++ config.def.h
@@ -5,8 +5,8 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "iosevka:pixelsize=16:antialias=true:autohint=true";
-static int borderpx = 3;
+static char *font = "monospace:pixelsize=13:antialias=true:autohint=true";
+static int borderpx = 2;
 
 /*
  * What program is execed by st depends of these precedence rules:
@@ -94,7 +94,7 @@ char *termname = "st-256color";
 unsigned int tabspaces = 8;
 
 /* bg opacity */
-float alpha = 0.9;
+float alpha = 0.85;
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
