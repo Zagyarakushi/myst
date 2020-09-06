8a6113d7cd732d2a9595ebd24b1cc9767aca07dc
zagyarakushi <zagyarakushi@gmail.com>
2020-09-06 15:03:18 +0100

    changed sh to bash and added font options

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "monospace:pixelsize=14";
+static char *font = "monospace:pixelsize=14:antialias=true:autohint=false";
 static int borderpx = 2;
 
 /*
